1a1f7a69c4f1b8a4405911fcc1fc6179c2d758b9
Vitalyastiy <118436864+Vitalyastiy@users.noreply.github.com>
2022-11-18 11:57:58 +0300

    Update and rename SCATERPLOT_2.py to .SCATERPLOT_2.py

--- .SCATERPLOT_2.py
+++ .SCATERPLOT_2.py
@@ -8,20 +8,17 @@ import seaborn as sns
 print("Setup Complete")
 # %%
 # Path of the file to read
-candy_filepath = (r"C:\Users\vitaly.flerin\Desktop\python\sea\candy.csv")
+candy_filepath = (r"C:\Users\....n\Desktop\python\sea\candy.csv")
 # %%
 # Read the file into a variable insurance_data
 candy_data = pd.read_csv(candy_filepath, index_col = 'id')  ##sep = ';'
-
 # %%
 candy_data.head()
-
 # %%
 sns.scatterplot(x=candy_data['sugarpercent'], y=candy_data['winpercent'])
 # %%
 sns.regplot(x=candy_data['sugarpercent'], y=candy_data['winpercent'])
 # %%
-
 sns.scatterplot(x=candy_data['pricepercent'], y=candy_data['winpercent'], hue=candy_data['chocolate'])  
 # %%
 sns.lmplot(x="pricepercent", y="winpercent", hue="chocolate", data=candy_data)
